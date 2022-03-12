d32d80690af16ccee60d17412fa5e1d0a43912b6
return0jz <jasperjzhou@gmail.com>
2022-03-13 05:55:58 +0800

    added registration stuff

--- api.py
+++ api.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for
+from hashlib import sha256
+
+patient_keys = ["GENESIS"]
 
 app = Flask(__name__)
 
@@ -6,6 +9,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['POST', 'GET'])
 def register():
-    return render_template("register.html")
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        new_key = sha256(patient_keys[-1].encode()).hexdigest()
+        patient_keys.append(new_key)
+        return new_key
