e7584d8c62f4578b4ff241e364ecc8503a09a2bc
return0jz <jasperjzhou@gmail.com>
2022-03-13 20:13:37 +0800

    Final

--- api.py
+++ api.py
@@ -1,15 +1,22 @@
+from crypt import methods
+import enum
 from re import T
 import os
 from flask import Flask, render_template, request, url_for
 from flask_mail import Mail, Message
 from hashlib import sha256
+import json
 
 patient_keys = ["GENESIS"]
-doctor_keys = [("GENESIS1", "GENESIS2")]
+doctor_keys = {"GENESISPRIVATE": "GENESISPUBLIC"}
 patient_db = {
     "GENESIS": { 
-        "records": [""],
-        "doctor_keys": [""]
+        "records": [],
+        "doctor_keys": []
+    }, 
+    "TEST": { 
+        "records": ["drive.google.com/profile.pdf"],
+        "doctor_keys": ["aksdjfldsa92edk0", "GENESISPUBLIC"]
     }, 
 }
 
@@ -43,7 +50,7 @@ def register():
         else:
             new_public_key = sha256(patient_keys[-1][0].encode()).hexdigest()
             new_private_key = sha256(patient_keys[-1][1].encode()).hexdigest()
-            doctor_keys.append((new_public_key, new_private_key))
+            doctor_keys[new_private_key] = new_public_key
             msg = Message("Your authentification key for Unirecords", sender="from@example.com", recipients=[data['email']])
             msg.body = f'Public key: {new_public_key}\nPrivate key: {new_private_key}'
             mail.send(msg)
@@ -58,8 +65,53 @@ def login_doctor():
 
 @app.route("/patient/<key>")
 def patient_view(key):
-    return render_template("patient_view.html")
+    try:
+        patient_data = patient_db[key]
+    except:
+        return "No user exists."
+    else:
+        return render_template("patient_view.html", data=patient_db[key],
+                                url1=f'/patient/{key}/perm', 
+                                url2=f'/patient/{key}/records', 
+                                url3=f'/patient/{key}/profile')
 
-@app.route("/patient/<key>/update")
-def patient_update(key):
-    return ("YES")
+@app.route("/patient/<key>/perm")
+def patient_perm(key):
+    return render_template("patient_perm.html", key=key, inp_keys=patient_db[key]['doctor_keys'])
+
+@app.route("/patient/<key>/records")
+def patient_records(key):
+    return render_template("patient_records.html", key=key, inp_records=patient_db[key]['records'])
+
+@app.route("/doctor/<key>")
+def doctor_view(key):
+    pubkey = doctor_keys[key]
+    patients_of_doctor = []
+    for i in patient_db:
+        if pubkey in patient_db[i]["doctor_keys"]:
+            patients_of_doctor.append(patient_db[i])
+    return render_template("doctor_view.html", patients=patients_of_doctor, enumerate=enumerate)
+
+@app.route("/api/updateRecords", methods=["POST"])
+def update_records():
+    data = request.json
+    try:
+        patient_db[data['patient_key']]
+    except:
+        return "fail"
+    else:
+        patient_db[data['patient_key']]['records'] = data['records']
+        print(patient_db)
+        return "success"
+
+@app.route("/api/updatePerm", methods=["POST"])
+def update_perm():
+    data = request.json
+    try:
+        patient_db[data['patient_key']]
+    except:
+        return "fail"
+    else:
+        patient_db[data['patient_key']]['doctor_keys'] = data['doctor_keys']
+        print(patient_db)
+        return "success"
